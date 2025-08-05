ff260d0274462ded488d160365b7fd598f360e56
sameerkindia <sameerkhanaa5@gmail.com>
2025-08-05 08:49:15 +0530

    models added

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -1,21 +1,30 @@
-require('./db/connect')
-const express = require('express')
-const app = express()
-const tasks = require('./routes/tasks')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require('dotenv').config()
 
-app.use(express.json())
-app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.static("./public"));
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
-app.get('/',(req,res)=>{
-    res.send("working")
-})
+app.use("/api/v1/tasks", tasks);
 
-app.use('/api/v1/tasks', tasks)
+const port = 5000;
 
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    console.log("✅ Database connected");
+    app.listen(port, () => {
+      console.log(`app running on server ${port}`);
+    });
+  } catch (err) {
+    console.error("❌ Connection error:", err.message);
+  }
+};
 
-const port = 5000
-
-app.listen(port, ()=>{
-    console.log(`app running on server ${port}`)
-})
+start();
